5bfd1751aeecd9c4a9b89917358d1ed2721b8807
nancychenxizhong <nanceeezhong@gmail.com>
2024-01-02 22:31:17 +1100

    collect relevant info in dataframe

--- price_fetching.py
+++ price_fetching.py
@@ -46,42 +46,59 @@ def googleShoppingSearch(product_to_search):
 
 	# get the necessary information from each result
 	for items in results:
-	    product_descprition= items.find_element(By.CLASS_NAME,'C7Lkve').find_element(By.CLASS_NAME,'EI11Pd').find_element(By.CLASS_NAME,'tAxDx').get_attribute('innerHTML')
-	    product_price = items.find_element(By.CLASS_NAME,'XrAfOe').find_element(By.CLASS_NAME,'kHxwFf').find_element(By.CLASS_NAME,'QIrs8').find_element(By.TAG_NAME,'span').get_attribute('innerHTML')
-	    product_price=product_price.replace('&nbsp;','')
-	    product_seller= items.find_element(By.CLASS_NAME,'aULzUe.IuHnof').get_attribute('innerHTML')
-	    shippment_conditions= items.find_element(By.CLASS_NAME,'bONr3b').find_element(By.CLASS_NAME,'vEjMR').get_attribute('innerHTML')
-	    shippment_conditions=shippment_conditions.replace('&nbsp;','')
-	    product_img_url = items.find_element(By.CLASS_NAME,'ArOc1c').find_element(By.TAG_NAME,'img').get_attribute('src')
-	    product_seller_url= items.find_element(By.CLASS_NAME,'eaGTj.mQaFGe.shntl').find_element(By.TAG_NAME,'div').find_element(By.TAG_NAME,'a').get_attribute('href')
-	    
+		product_descprition= items.find_element(By.CLASS_NAME,'C7Lkve').find_element(By.CLASS_NAME,'EI11Pd').find_element(By.CLASS_NAME,'tAxDx').get_attribute('innerHTML')
+		product_price = items.find_element(By.CLASS_NAME,'XrAfOe').find_element(By.CLASS_NAME,'kHxwFf').find_element(By.CLASS_NAME,'QIrs8').find_element(By.TAG_NAME,'span').get_attribute('innerHTML')
+		product_price=product_price.replace('&nbsp;','')
+		#product_seller= items.find_element(By.CLASS_NAME,'aULzUe.IuHnof').get_attribute('innerHTML')
+		product_seller= items.find_element(By.CLASS_NAME, 'aULzUe.IuHnof').get_attribute('innerText')
+		shippment_conditions= items.find_element(By.CLASS_NAME,'bONr3b').find_element(By.CLASS_NAME,'vEjMR').get_attribute('innerHTML')
+		shippment_conditions=shippment_conditions.replace('&nbsp;','')
+		product_img_url = items.find_element(By.CLASS_NAME,'ArOc1c').find_element(By.TAG_NAME,'img').get_attribute('src')
+		product_seller_url= items.find_element(By.CLASS_NAME,'eaGTj.mQaFGe.shntl').find_element(By.TAG_NAME,'div').find_element(By.TAG_NAME,'a').get_attribute('href')
+		
 
-	    # add the information found to each list
-	    product_searched_list.append(product_to_search)
-	    product_descprition_list.append(product_descprition)
-	    product_price_list.append(product_price)
-	    product_seller_list.append(product_seller)
-	    shippment_conditions_list.append(shippment_conditions)
-	    product_img_url_list.append(product_img_url)
-	    product_seller_url_list.append(product_seller_url)
-	    search_date_list.append(date.today())
+		# add the information found to each list
+		product_searched_list.append(product_to_search)
+		product_descprition_list.append(product_descprition)
+		product_price_list.append(product_price)
+		product_seller_list.append(product_seller)
+		shippment_conditions_list.append(shippment_conditions)
+		product_img_url_list.append(product_img_url)
+		product_seller_url_list.append(product_seller_url)
+		search_date_list.append(date.today())
 
 	product_search_df = pd.DataFrame({"product searched":product_searched_list,"product found description":product_descprition_list,
-	                "product found price":product_price_list,"product found seller":product_seller_list,
-	                "product found shippment conditions":shippment_conditions_list,
-	                "product found seller url":search_date_list,
-	                "product found shippment img URL":product_img_url_list})
+					"product found price":product_price_list,"product found seller":product_seller_list,
+					"product found shippment conditions":shippment_conditions_list,
+					"product found seller url":search_date_list,
+					"product found shippment img URL":product_img_url_list})
 
 	#print(product_search_df)
 	return product_search_df
 
+def check_product_name_price_conditions(row, product_name):
+	# print(product_name)
+	# print(row['product found description'].lower())
+	description_match = product_name.lower() in row['product found description'].lower()
+	price_cond_match = 'Was' in row['product found price']
+	# print('d', description_match)
+	# print('p', price_cond_match)
+	return description_match and price_cond_match
+
 if __name__ == "__main__":
-	
+	product_name = "nature's way beauty collagen shot"
 	shops = ['chemist warehouse', 'coles', 'woolworths', 'priceline']
+	df_save = pd.DataFrame(columns=['product found description', 'product found price', 'product found seller'])
 	for shop in shops:
-		res_df = googleShoppingSearch(f"nature's way collagen shot {shop}")
-		print(res_df[['product found description', 'product found price', 'product found seller']].loc[:5])
+		res_df = googleShoppingSearch(f"{product_name} {shop}")
+		#print(res_df[['product found description', 'product found price', 'product found seller']].loc[:5])
 
-
-	#print(res.head())
-	#print(res[['product found description', 'product found price', 'product found seller']])
+		res_df_portion = res_df[['product found description', 'product found price', 'product found seller']].loc[:5]
+		res_df_portion['matches'] = res_df_portion.apply(lambda row: check_product_name_price_conditions(row, product_name), axis=1)
+		#print(res_df_portion['matches'])
+		res_df_portion_filtered = res_df_portion[res_df_portion['matches']]
+		#print(res_df_portion_filtered)
+		df_save = pd.concat([df_save, res_df_portion_filtered]).reset_index(drop=True)
+		df_save.drop_duplicates(inplace=True)
+		
+	print(df_save)
